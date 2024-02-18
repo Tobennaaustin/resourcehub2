93ce92381b20318e0d23eef6974f8b90f6f0d4ba
tobenna <tobennaaustin@gmail.com>
2024-02-18 11:36:48 -0100

    i made some changes

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
 const open = document.getElementById("open");
-    const close = document.getElementById("close");
-    const aside = document.querySelector("aside");
+const close = document.getElementById("close");
+const aside = document.querySelector("aside");
+const acc = document.getElementById("acc");
 
-    open.addEventListener('click', () => {
-      // aside.style.width ="70%";
-      aside.style.display = "block";
-      aside.style.transition = ".10s ease"
-  
-    });
-    close.addEventListener('click', () => {
-      // aside.style.width = "0"
-      aside.style.display = "none";
-      
-    });
+open.addEventListener('click', () => {
+  // aside.style.width ="70%";
+  aside.style.display = "block";
+  aside.style.transition = ".10s ease"
 
-    if(window.screen.width < 799){
-      aside.style.display = "none"
-    }
+});
+close.addEventListener('click', () => {
+  // aside.style.width = "0"
+  aside.style.display = "none";
+
+});
+
+if (window.screen.width < 799) {
+  aside.style.display = "none"
+}
+
+
+
+// acc.addEventListener('click', () => {
+//   window.location.href = 'account.html';
+// });
